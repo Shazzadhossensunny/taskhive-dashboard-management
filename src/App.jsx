25bf07158f795f147b442d25a85c505d2ff2796d
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-06-17 01:00:01 +0600

    Fix routing for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -11,43 +11,37 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import TaskDetailPage from "./pages/TaskDetailPage";
 
-// Create ProtectedRoute as a separate component inside AppProvider
 const ProtectedRoute = ({ children }) => {
   const { user } = useAppContext();
   return user ? children : <Navigate to="/" replace />;
 };
 
-function AppContent() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/task/:id"
-          element={
-            <ProtectedRoute>
-              <TaskDetailPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
-}
-
 function App() {
   return (
     <AppProvider>
-      <AppContent />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/task/:id"
+            element={
+              <ProtectedRoute>
+                <TaskDetailPage />
+              </ProtectedRoute>
+            }
+          />
+          {/* Add a catch-all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </AppProvider>
   );
 }
